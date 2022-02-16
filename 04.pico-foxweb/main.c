b2c5604463709ce0db911be88ba3cd362e8e60dc
Dmitrii Vikhliaev <dim49v@yandex.ru>
2022-02-17 01:55:21 +0300

    fixed log

--- 04.pico-foxweb/main.c
+++ 04.pico-foxweb/main.c
@@ -38,6 +38,7 @@ int read_file(const char *file_name) {
       fwrite(buf, 1, nread, stdout);
 
     err = ferror(file);
+    sprintf(logMessage, "%s %ld",logMessage, ftell(file));
     fclose(file);
   }
   return err;
@@ -46,20 +47,19 @@ int read_file(const char *file_name) {
 void route() {
   ROUTE_START()
 
-  GET("/") {
+  GET("/")
     char index_html[40];
     sprintf(index_html, "%s%s", public_dir, INDEX_HTML);
 
-    HTTP_200;
+    HTTP_200
     if (file_exists(index_html)) {
       read_file(index_html);
     } else {
       printf("Hello! You are using %s\n\n", request_header("User-Agent"));
     }
-  }
 
-  GET("/test") {
-    HTTP_200;
+  GET("/test")
+    HTTP_200
     printf("List of request headers:\n\n");
 
     header_t *h = request_headers();
@@ -68,22 +68,20 @@ void route() {
       printf("%s: %s\n", h->name, h->value);
       h++;
     }
-  }
 
-  POST("/") {
-    HTTP_201;
+  POST("/")
+    HTTP_201
     printf("Wow, seems that you POSTed %d bytes.\n", payload_size);
     printf("Fetch the data using `payload` variable.\n");
     if (payload_size > 0)
       printf("Request body: %s", payload);
-  }
 
-  GET(uri) {
+  GET(uri)
     char file_name[255];
     sprintf(file_name, "%s%s", public_dir, uri);
 
     if (file_exists(file_name)) {
-      HTTP_200;
+      HTTP_200
       read_file(file_name);
     } else {
       HTTP_404;
@@ -91,7 +89,6 @@ void route() {
       if (file_exists(file_name))
         read_file(file_name);
     }
-  }
-
+  
   ROUTE_END()
 }
